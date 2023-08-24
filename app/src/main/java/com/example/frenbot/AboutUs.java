ecad28ba22db8c7d63b1a8088d359d37fcf35b3c
Nirjhar <nirjharsingha@gmail.com>
2023-08-24 10:52:07 +0600

    authentication, basic layout of dashboard, event, sos, academia, about us and related intent are added

--- app/src/main/java/com/example/frenbot/AboutUs.java
+++ app/src/main/java/com/example/frenbot/AboutUs.java
@@ -1,8 +1,8 @@
 package com.example.frenbot;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutUs extends AppCompatActivity {
 
--- app/src/main/java/com/example/frenbot/Signup.java
+++ app/src/main/java/com/example/frenbot/Signup.java
@@ -1,21 +1,34 @@
 package com.example.frenbot;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Signup extends AppCompatActivity {
 
     Button login,signup;
     FloatingActionButton fb,google,linkedin;
     LinearLayout ll;
+    TextInputEditText mail, username, password, cnPassword;
+    FirebaseAuth mAuth;
     float v=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +38,13 @@ public class Signup extends AppCompatActivity {
         fb = findViewById(R.id.fb);
         google = findViewById(R.id.google);
         linkedin = findViewById(R.id.linkedin);
-        ll=findViewById(R.id.ll);
-        signup=findViewById(R.id.signup);
+        ll = findViewById(R.id.ll);
+        signup = findViewById(R.id.signup);
+        mail = findViewById(R.id.mail);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        cnPassword = findViewById(R.id.cnPassword);
+        mAuth = FirebaseAuth.getInstance();
 
         fb.setTranslationY(300);
         google.setTranslationY(300);
@@ -46,15 +64,72 @@ public class Signup extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(Signup.this, Login.class);
                 startActivity(intent);
-                finish();
             }
         });
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Signup.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                String email, userName, pass, confirmPass;
+                email = String.valueOf(mail.getText());
+                userName = String.valueOf(username.getText());
+                pass = String.valueOf(password.getText());
+                confirmPass = String.valueOf(cnPassword.getText());
+
+                if(TextUtils.isEmpty(email)) {
+                    Toast.makeText(Signup.this,"Enter email",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(userName)) {
+                    Toast.makeText(Signup.this,"Enter user name",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)) {
+                    Toast.makeText(Signup.this,"Enter password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(confirmPass)) {
+                    Toast.makeText(Signup.this,"Enter confirm password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!pass.equals(confirmPass)) {
+                    Toast.makeText(Signup.this,"Invalid confirm password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    if (user != null) {
+                                        String userId = user.getUid();
+
+                                        DatabaseReference usersRef = FirebaseDatabase.getInstance("https://frenbot-ebcbe-default-rtdb.asia-southeast1.firebasedatabase.app")
+                                                .getReference("users");
+
+                                        usersRef.child(userId).child("username").setValue(userName).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(Signup.this, "Account created and database updated.", Toast.LENGTH_SHORT).show();
+                                                    Intent intent = new Intent(Signup.this, MainActivity.class);
+                                                    startActivity(intent);
+                                                } else {
+                                                    Toast.makeText(Signup.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                                    Log.e("DatabaseUpdate", "Failed to update database: " + task.getException());
+                                                }
+                                            }
+                                        });
+                                    }
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Signup.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    Log.e("UserCreation", "Failed to create user: " + task.getException());
+                                }
+                            }
+                        });
+
             }
         });
         fb.setOnClickListener(new View.OnClickListener() {
